56a02da0fbaa9b3e48ea6890b167d6159ab5b929
peterwhanson <peterhanson@email.arizona.edu>
2014-11-09 04:09:23 -0700

    Update Player.java

--- src/client/Player.java
+++ src/client/Player.java
@@ -1,1 +1,64 @@
+package client;
+
+import java.io.Serializable;
+
 //Created the Player.java class in right folder hopefully
+
+
+public class Player implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -937778023185001965L;
+	private String name;
+	private int money;
+	private int healthPoints;
+	
+	public Player(String name, int money, int healthPoints){
+		this.name = name;
+		this.money = money;
+		this.healthPoints = healthPoints;
+	}
+
+	public String getName(){
+		return name;
+	}
+	
+	public int getMoney(){
+		return money;
+	}
+	
+	public void setMoney(int amount){
+		money = amount;
+	}
+	
+	public void setHealth(int amount){
+		healthPoints = amount;
+	}
+	
+	
+	public int getHealthPoints(){
+		return healthPoints;
+	}
+	
+	
+	public void loseHealth(int hpLost) {
+		healthPoints = healthPoints - hpLost;
+	}
+	
+	public void spendMoney(int moneySpent){
+		money = money - moneySpent;
+	}
+	
+	public boolean isAlive(){
+		if(healthPoints > 0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	
+	
+}
