82529f2f580f0b8d22446c4f9f88f295b26710f7
dburnsii <dburnsii@email.arizona.edu>
2014-11-15 22:31:06 -0700

    Revert "Revert "Added tower drag and drop""
    This reverts commit e275e92354851173a3e2b647db08b3231abe2abf.

---
+++ src/GUI/map1Path.java
@@ -0,0 +1,84 @@
+package GUI;
+
+import java.awt.Point;
+import java.awt.geom.Point2D;
+
+public class map1Path
+{
+	public Point2D.Double getLocation(double progress)
+	{
+		if(progress <= 6)
+		{
+			return new Point2D.Double(progress, 1);
+		}
+		else if(progress <= 11)
+		{
+			return new Point2D.Double(6, progress - 5);
+		}
+		else if(progress <= 14)
+		{
+			return new Point2D.Double((-progress) + 17, 6);
+		}
+		else if(progress <= 15)
+		{
+			return new Point2D.Double(3, (-progress) + 20);
+		}
+		else if(progress <= 16)
+		{
+			return new Point2D.Double(progress - 12, 5);
+		}
+		else if(progress <= 19)
+		{
+			return new Point2D.Double(4, (-progress) + 21);
+		}
+		else if(progress <= 26)
+		{
+			return new Point2D.Double((-progress) + 23, 2.5);
+		}
+		else if(progress <= 33)
+		{
+			
+		}
+		else if(progress <= 50)
+		{
+			
+		}
+		else if(progress <= 52)
+		{
+			
+		}
+		else if(progress <= 62)
+		{
+			
+		}
+		else if(progress <= 64)
+		{
+			
+		}
+		else if(progress <= 74)
+		{
+			
+		}
+		else if(progress <= 76)
+		{
+			
+		}
+		else if(progress <= 86)
+		{
+			
+		}
+		else if(progress <= 88)
+		{
+			
+		}
+		else if(progress <= 98)
+		{
+			
+		}
+		else if(progress <= 100)
+		{
+			
+		}
+		return new Point2D.Double(0,0);
+	}
+}
