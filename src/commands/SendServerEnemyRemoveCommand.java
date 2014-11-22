3f8ed23d22307494562a42d3d7a3714f26d0e453
Brody Painter <brodypainter@email.arizona.edu>
2014-11-22 16:58:49 -0700

    Client addTower now takes a tower and point, implemented Client removeTower, implemented Client side removeEnemy

--- src/commands/SendServerEnemyRemoveCommand.java
+++ src/commands/SendServerEnemyRemoveCommand.java
@@ -11,6 +11,10 @@ public class SendServerEnemyRemoveCommand extends Command<GameServer>{
 	private static final long serialVersionUID = 5421561357860369854L;
 	private Enemy enemy;
 
+	public SendServerEnemyRemoveCommand(Enemy e){
+		this.enemy = e;
+	}
+	
 	@Override
 	public void execute(GameServer executeOn) {
 		executeOn.removeEnemy(this.enemy);
