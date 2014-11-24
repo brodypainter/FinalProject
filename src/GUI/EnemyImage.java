f7ca663ff333142f02f6d038e6ba2eb348ef4bdb
peterwhanson <peterhanson@email.arizona.edu>
2014-11-23 17:07:30 -0700

    Create EnemyImage.java

---
+++ src/GUI/EnemyImage.java
@@ -0,0 +1,31 @@
+package GUI;
+
+import java.awt.Point;
+import java.io.Serializable;
+
+import GameController.Enemy;
+import GameController.Enemy.directionFacing;
+
+public class EnemyImage implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4456803491165705554L;
+	
+	private String imageURL; //The URL of the image to paint
+	private Point location; //The coordinates (rowsdown, column) in the grid to paint image
+	private directionFacing orientation; //The orientation NORTH SOUTH EAST WEST enemy faces
+	private int progress; //The percentage that enemy is across the tile. 0 to 100
+						  //Use this to offset the enemy from its top left corner of grid location
+	private int healthPercentage; //For future use to display HP bar of enemies, 0 to 100
+
+	public EnemyImage(Enemy enemy){
+		this.imageURL = enemy.getImageURL();
+		this.location = enemy.getLocation();
+		this.orientation = enemy.getOrientation();
+		this.progress = enemy.getProgress();
+		this.healthPercentage = enemy.getHealthPercentage();
+	}
+
+}
