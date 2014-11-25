20f9d0427035f13c0f505ebfa7362aa2ffa7ef2a
brodypainter <brodypainter@email.arizona.edu>
2014-11-24 19:24:30 -0700

    Removed createLevel call in clientAccepter

--- src/server/GameServer.java
+++ src/server/GameServer.java
@@ -116,7 +116,6 @@ public class GameServer implements Serializable{
 					// create the single player, will need to change this for multiplayer games
 					// for multiplayer, this will need to check if the player already exists
 					player = new Player(clientName, 100, 100);
-					createLevel(0);
 					
 					System.out.println("Level is: " + currentLevel.toString());
 					
