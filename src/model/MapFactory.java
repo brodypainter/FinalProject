7d0b1c38ae5b3386d9cf3c25394138df71402250
peterwhanson <peterhanson@email.arizona.edu>
2014-11-25 00:02:27 -0700

    image urls added

--- src/model/MapFactory.java
+++ src/model/MapFactory.java
@@ -79,7 +79,7 @@ public class MapFactory {
 			/**
 			 * @ Max Justice Sun 10:50 added ceruleanGym
 			 */
-			backgroundImage = "src/images/Cerulean_Gym.png"; //TODO:Desone, make an image of this 5 row x 10 column map with path straight across middle
+			backgroundImage = "src/images/map0.png"; //TODO:Desone, make an image of this 5 row x 10 column map with path straight across middle
 									//and place the image's location here <---
 			
 			//instantiate a LevelXMap object that extends Map class
