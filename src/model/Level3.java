0cd3ec4b3cc044a26dc024e3dcb8bb33009fb7dd
Max Justice <mbjustice07@github.com>
2014-12-02 21:35:39 -0700

    tes

---
+++ src/model/Level3.java
@@ -0,0 +1,68 @@
+package model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import GameController.Enemy;
+import GameController.Pikachu;
+import server.GameServer;
+import client.Player;
+
+public class Level3 extends Level implements Serializable{
+	
+	private static final int numbOfWaves = 8;
+
+
+	public Level3(Player player, GameServer server) {
+		super(player, server);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createWaves() {
+		// TODO Auto-generated method stub
+		ArrayList<ArrayList<Enemy>> waveList = new ArrayList<ArrayList<Enemy>>(); //A temporary 2D array list of Enemy
+		for (int i = 0; i < 10; i++){
+			ArrayList<Enemy> wave = new ArrayList<Enemy>();
+			for (int j = 0; j < 8; j++){
+				Pikachu pika = new Pikachu(getMap());
+				wave.add(pika);
+			}
+			waveList.add(wave);
+		}
+		setWavesList(waveList);
+	}
+
+	@Override
+	public void setPlayerStartingHP() {
+		// TODO Auto-generated method stub
+		getPlayer().setHealth(100);
+		notifyPlayerInfoUpdated();
+	}
+
+	@Override
+	public void setPlayerStartingMoney() {
+		// TODO Auto-generated method stub
+		getPlayer().setMoney(1000);
+		notifyPlayerInfoUpdated();
+	}
+
+	@Override
+	public void setWaveDelayIntervals() {
+		setWaveIntervals(15000L); //15 seconds between the last and first enemy of 2 successive waves
+	}
+
+	@Override
+	public void setEnemySpawnDelayIntervals() {
+		setEnemySpawnIntervals(1500L); //2 second between each enemy spawning in a wave
+	}
+
+	@Override
+	public void setMap() {
+		Map levelsMap = MapFactory.generateMap(getPlayer(), 0);
+		 //the int mapCode is 0 because this is level 0 and we want Map 0
+		levelsMap.setServer(this.getServer()); //Must set the map's server so it knows to send first update
+		this.setMap(levelsMap);							//and where to send updates thereafter
+	}
+
+}
